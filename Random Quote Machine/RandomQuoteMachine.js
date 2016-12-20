69c902475ffac072c7285ae1e425520425ba3281
spences10 <spences10apps@gmail.com>
2016-12-20 21:43:32 +0000

    arnold json

--- Random Quote Machine/RandomQuoteMachine.js
+++ Random Quote Machine/RandomQuoteMachine.js
@@ -1,23 +1,57 @@
- $(document).ready(function() {
+// $(document).ready(function () {
 
-     $("#newQuote").click(function() {
+//     $('#newQuote').click(function () {
 
-         alert("you clicked");
-     });
+//         const endpoint = 'https://gist.githubusercontent.com/spences10/ceee092f6fed36559036c94682b7a5f7/raw/1cbf8fcff45f0a0da9e5d4792c84753d20567aa7/arnold_quote_api.json'
 
-     $("#newImage").click(function() {
+//         const getJSON = (endpoint, cb) => fetch(url)
+//             .then(response => response.json())
+//             .then(json => cb(null, json))
+//             .catch(cb);
 
-         alert("you clicked");
-     });
-     $("#tweetQuote").click(function() {
+//     });
+// });
 
-         alert("you clicked");
-     });
- });
+// $(document).ready(function () {
 
+//     $("#newQuote").click(function () {
 
 
+//     });
 
- // newQuote
- // newImage
- // tweetQuote
+//     $("#newImage").click(function () {
+
+//         alert("you clicked");
+//     });
+//     $("#tweetQuote").click(function () {
+
+//         alert("you clicked");
+//     });
+// });
+
+
+// function getQuotes() {
+
+//     const endpoint = 'https://gist.githubusercontent.com/spences10/ceee092f6fed36559036c94682b7a5f7/raw/1cbf8fcff45f0a0da9e5d4792c84753d20567aa7/arnold_quote_api.json'
+
+//     const quotes = [];
+
+//     const prom = fetch(endpoint).then(blob => console.log(blob));
+
+//     // fetch(endpoint)
+//     //     .then(blob => blob.json())
+//     //     .then(data => quotes.push(...data));
+
+//     // console.log(quotes);
+//     // return quotes;
+// }
+
+// function processQuotes(quotes) {
+//     console.log(quotes);
+// }
+
+// arnold https://gist.githubusercontent.com/spences10/ceee092f6fed36559036c94682b7a5f7/raw/7a27570759834ee454ee380ca42ebd47dc55e932/arnold_quotes.json
+// newQuote
+// newImage
+// tweetQuote
+// cities https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json
