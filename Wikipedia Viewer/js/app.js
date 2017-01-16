f2676be05027b630dd32264107aa8223861e66a7
spences10 <spences10apps@gmail.com>
2017-01-16 20:08:23 +0000

    Wikipedia viewer Complete jQuery append with nice page layout  :+1:

--- Wikipedia Viewer/js/app.js
+++ Wikipedia Viewer/js/app.js
@@ -1,30 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-		$('.search').keyup(function(event){
-			var keyword = $(this).val();
-			if(event.keyCode == 13){
-				getSearchResults(keyword);
-			}
-		});
+	$('.search').keyup(function (event) {
+		var keyword = $(this).val();
+		if (event.keyCode == 13) {
+			getSearchResults(keyword);
+		}
 	});
+});
 
-	function getSearchResults(val){
-		var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
-		var page='https://en.wikipedia.org/?curid=';
+function getSearchResults(val) {
+	var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+	var page = 'https://en.wikipedia.org/?curid=';
 
-		$.get(api+val,function(data){
-			var pages = data.query.pages;
-			//console.log(pages);
-			$('.list').empty();
-			for(var p in pages){
-				if(pages.hasOwnProperty(p)){
-					//console.log(pages[p].title);
-
-
-
-					$('.list').append('<div class="result"><li><a href="'+page+pages[p].pageid+'" target="_blank">'+pages[p].title+'<br><br><p id="des">'+pages[p].extract+'<p></a></li></div><br>');
-				}
+	$.get(api + val, function (data) {
+		var pages = data.query.pages;
+		//console.log(pages);
+		$('.list').empty();
+		for (var p in pages) {
+			if (pages.hasOwnProperty(p)) {
+				//console.log(pages[p].title);
+				$('.list').append(
+					'<div class="search-row row row-centered">' +
+					'	<div class="col-xs-10 col-centered">' +
+					'		<a href="' + page + pages[p].pageid + '" target="_blank">' +
+					'			<div class="panel panel-default">' +
+					'				<div class="panel-heading">' +
+					'					<h3 class="panel-title">' + pages[p].title + '</h3>' +
+					'				</div>' +
+					'				<div class="panel-body">' +
+					'					' + pages[p].extract + 
+					'				</div>' +
+					'			</div>' +
+					'		</a>' +
+					'	</div>' +
+					'</div>');
 			}
+		}
 
-		},"jsonp");
-	}
+	}, "jsonp");
+}
