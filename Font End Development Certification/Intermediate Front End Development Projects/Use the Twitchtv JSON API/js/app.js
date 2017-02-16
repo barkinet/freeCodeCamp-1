1c451085f974689e8b20782c0934bd5aed3e8392
spences10 <spences10apps@gmail.com>
2017-02-16 07:23:51 +0000

    plain js page events some css, some html

---
+++ Font End Development Certification/Intermediate Front End Development Projects/Use the Twitchtv JSON API/js/app.js
@@ -0,0 +1,14 @@
+const channels = [
+  'Joltzdude139', 'ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx',
+  'RobotCaleb', 'noobs2ninjas'
+]
+let endpoint = 'https://wind-bow.gomix.me/twitch-api//channels/' + channels
+
+console.log(endpoint)
+console.log(document.readyState)
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log(document.readyState)
+  // add things
+})
+
