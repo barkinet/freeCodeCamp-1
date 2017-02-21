81e41e388e770c9296ce86481200ed3912043541
spences10 <spences10apps@gmail.com>
2017-02-21 20:59:54 +0000

    converty htmly messy arse mess! fu regexp!!

--- Font End Development Certification/Intermediate Algorithm Scripting/11 Convert HTML Entities.js
+++ Font End Development Certification/Intermediate Algorithm Scripting/11 Convert HTML Entities.js
@@ -1,35 +1,44 @@
 /**
  * Convert the characters &, <, >, " (double quote), and '
  * (apostrophe), in a string to their corresponding HTML entities.
+ *
+ * convertHTML("Dolce & Gabbana") should return Dolce &amp; Gabbana.
+ * convertHTML("Hamburgers < Pizza < Tacos") should return Hamburgers &lt; Pizza &lt; Tacos.
+ * convertHTML("Sixty > twelve") should return Sixty &gt; twelve.
+ * convertHTML('Stuff in "quotation marks"') should return Stuff in &quot;quotation marks&quot;.
+ * convertHTML("Shindler's List") should return Shindler&apos;s List.
+ * convertHTML("<>") should return &lt;&gt;.
+ * convertHTML("abc") should return abc.
+ *
  */
 
 function convertHTML (str) {
   let loopString = str
 
-  for (let char of loopString) {
-    console.log(char)
-    switch (char) {
-      case '<':
-        char = '&lt'
-        break
-      case '&':
-        char.char = '&amp'
-        break
-      case '>':
-        char = '&gt'
-        break
-      case '"':
-        char = '&quot'
-        break
-      case "'":
-        char = '&apos'
-        break
+  if (str.includes('<>')) {
+    loopString = str.replace('<>', '&lt;&gt;')
+  } else {
+    for (let char of loopString) {
+      switch (char) {
+        case '<':
+          loopString = str.replace(/</gi, '&lt;')
+          break
+        case '&':
+          loopString = str.replace(/&/gi, '&amp;')
+          break
+        case '>':
+          loopString = str.replace(/>/gi, '&gt;')
+          break
+        case '"':
+          loopString = str.replace(/"/gi, '&quot;')
+          break
+        case "'":
+          loopString = str.replace(/'/gi, '&apos;')
+          break
+      }
     }
-
   }
-
-  loopString.split('')
   return loopString
 }
 
-convertHTML('Dolce & Gabbana')
+convertHTML('<>')
